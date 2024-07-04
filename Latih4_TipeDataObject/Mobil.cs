60d909472f9c860bb8da128b61b23e8efc0844ec
buble <sucsespart1@gmail.com>
2024-07-04 10:32:30 +0700

    Add files via upload

---
+++ Latih4_TipeDataObject/Mobil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latih4_TipeDataObject
+{
+    public class Mobil
+    {
+        public string TipeMObil {  get; set; }
+        public string Warna { get; set; }
+        public int VolumeMesin { get; set; }
+        public string BahanBakar { get; set; }
+        public int MaxKecepatan { get; set; }
+        public DateTime TglProduksi { get; set; }
+    }
+}
---
+++ Latih4_TipeDataObject/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Latih4_TipeDataObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var BMW = new Mobil
+            {
+                TipeMObil = "BMW sedan",
+                Warna = "Putih",
+                VolumeMesin = 2500,
+                MaxKecepatan = 180,
+                BahanBakar = "Pertamax Turbo",
+                TglProduksi = new DateTime(2020, 1, 2)
+            };
+            Console.WriteLine($"Tipe Mobil: {BMW.TipeMObil}");
+            Console.WriteLine($"Warna: {BMW.Warna}");
+            Console.WriteLine($"Volume Mesin: {BMW.VolumeMesin}");
+            Console.WriteLine($"Max Kecepatan: {BMW.MaxKecepatan}");
+            Console.WriteLine($"Bahan Bakar: {BMW.BahanBakar}");
+            Console.WriteLine($"TglProduksi: {BMW.TglProduksi.ToString("dd-MM-yyyy")}");
+
+        }
+    }
+}
