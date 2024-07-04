cd4241534000937e047faf98c6f9cc1ee4de3954
buble <sucsespart1@gmail.com>
2024-07-04 10:29:15 +0700

    Add files via upload

---
+++ Latih2_TipeDataJamak/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Latih2_TipeDataJamak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> nilaimurid = new Dictionary<string, int>
+            {
+                { "Budi", 86 },
+                { "Siti", 91 },
+                { "Adit", 100 }
+            };
+            Console.WriteLine("Nilai Budi: " + nilaimurid ["Budi"]);
+            Console.WriteLine("Nilai Siti: " + nilaimurid ["Siti"]);
+            Console.WriteLine("Nilai Adit: " + nilaimurid ["Adit"]);
+            nilaimurid["Mamad"] = 95;
+            Console.WriteLine("Nilai Mamad: " + nilaimurid["Mamad"]);
+        }
+    }
+}
