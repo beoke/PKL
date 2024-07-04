585e8275d1dc6b8feb609e6f824fa313157dfff6
buble <sucsespart1@gmail.com>
2024-07-04 10:31:07 +0700

    Add files via upload

---
+++ Latih3_CollectionTest/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Latih3_CollectionTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> NamaSiswa = new List<string>
+        {
+            "Bram", "Yoga", "Ani", "Budi", "Adit"
+        };
+
+            Console.WriteLine("Daftar nama siswa:");
+            for (int i = 0; i < NamaSiswa.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {NamaSiswa[i]}");
+            }
+        }
+    }
+}
