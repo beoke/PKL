d4cb779a4435b6630bdc3313514ff7f97ba85528
buble <sucsespart1@gmail.com>
2024-07-08 22:11:13 +0700

    Add files via upload

---
+++ Latih9_ProsedurFungsi/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Latih9_ProsedurFungsi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //latihan 1
+            TampilkanWaktu();
+
+            //latihan 2
+            int a = 7;
+            int b = 6;
+            int hasil = kali(a,b);
+            Console.WriteLine("latihan 2");
+            Console.WriteLine("hasil Perkalian 6 * 7 adalah " + hasil);
+            Console.WriteLine();
+
+            //latihan 3
+            Console.WriteLine("latihan 3");
+            Console.WriteLine("Masukkan angka");
+            int c = int.Parse(Console.ReadLine());
+            bool GenapAtauGanjil= ApakahGenap(c);
+            Console.WriteLine("Angka tersebut "+ GenapAtauGanjil);
+        }
+
+        static void TampilkanWaktu() //latihan 1
+        {
+            Console.WriteLine("latihan 1");
+            DateTime waktuSekarang = DateTime.Now;
+            Console.WriteLine("Waktu saat ini adalah " + waktuSekarang);
+            Console.WriteLine();
+        }
+
+        static int kali(int a, int b) // latihan 2
+        {
+            return a * b;
+        }
+
+        static bool ApakahGenap(int x)
+        {
+           return x % 2 == 0;  
+        }
+    }
+}
